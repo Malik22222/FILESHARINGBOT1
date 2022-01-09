90a640ebd5d2a104b950e5a94471d99212a9f8b2
Leonardo Da Vinci <84631921+leanardo7994@users.noreply.github.com>
2022-01-09 18:24:58 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -95,18 +95,19 @@ async def start_command(client: Client, message: Message):
                 ]
             ]
         )
-        await message.reply(
-        text = FORCE_MSG.format(
+        await message.reply_text(
+            text = START_MSG.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
                 username = None if not message.from_user.username else '@' + message.from_user.username,
                 mention = message.from_user.mention,
                 id = message.from_user.id
             ),
-        reply_markup = InlineKeyboardMarkup(buttons),
-        quote = True,
-        disable_web_page_preview = True
-    )     
+            reply_markup = reply_markup,
+            disable_web_page_preview = True,
+            quote = True
+        )
+        return
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
@@ -132,8 +133,14 @@ async def not_joined(client: Client, message: Message):
                         ]
                     )
     await message.reply(
-        text = text,
-        reply_markup = reply_markup,
+        text = FORCE_MSG.format(
+                first = message.from_user.first_name,
+                last = message.from_user.last_name,
+                username = None if not message.from_user.username else '@' + message.from_user.username,
+                mention = message.from_user.mention,
+                id = message.from_user.id
+            ),
+        reply_markup = InlineKeyboardMarkup(buttons),
         quote = True,
         disable_web_page_preview = True
     )
